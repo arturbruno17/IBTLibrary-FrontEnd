93ff195bdf68b08ceb961a40ebcc5c845ea1fe79
Artur Bruno <arturjuliao20@gmail.com>
2025-04-18 12:12:46 -0300

    feat: get api base url from environment

--- src/services/api.ts
+++ src/services/api.ts
@@ -19,7 +19,7 @@ export const handleUnauthorized = () => {
 
 // Base API instance
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json'
     }
